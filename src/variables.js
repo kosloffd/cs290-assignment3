ec6c1c9a7b8ab29f8aa37f522b931d88b4e8fd94
kosloffd <kosloffd@onid.oregonstate.edu>
2015-04-19 12:51:29 -0700

    Spent one hour finishing hte variables task.  Most of the time was spent on syntax errors and finding the .toString() function.

--- src/variables.js
+++ src/variables.js
@@ -9,12 +9,15 @@ yourNumberIs: a string that says "Your Number is `x`." where `x` is replaced by
 in input `a`
 a: the original a number
 */
-function variableModification(a) {
+function variableModification(a) 
+{
   var plus5;
   var asString;
   var yourNumberIs;
   //your code here
-
+  plus5 = a + 5;
+  asString = a.toString();
+  yourNumberIs = "Your Number is " + a;
   //end your code
   return [plus5, asString, yourNumberIs, a];
 }
@@ -27,9 +30,12 @@ Output:
 return true if b is a primitive string value (also known as a string literal),
 false otherwise
 */
-function isString(b) {
+function isString(b) 
+{
   //your code here
-  return undefined;
+  var isStringLiteral;
+  typeof b === "string" ? isStringLiteral = true : isStringLiteral = false;
+  return isStringLiteral;
   //end your code
 }
 
@@ -40,8 +46,11 @@ c: could be anything
 Output:
 return true if c is null, false otherwise
 */
-function isNull(c) {
+function isNull(c) 
+{
   //your code here
-  return undefined;
+  var ifNull; 
+  c === null ? ifNull = true : ifNull = false;
+  return ifNull;
   //end your code
 }
